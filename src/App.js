20f016c7de650d57d353bdac484b973592dfc119
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-10-07 10:31:04 -0700

    app started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from '../src/contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -8,35 +9,30 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = item => {
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  return (
+    <div className='App'>
+      <ProductContext.Provider value={{}}>
+        <Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+        {/* Routes */}
+        <Route
+          exact
+          path='/'
+          render={() => <Products products={products} addItem={addItem} />}
+        />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+        <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
