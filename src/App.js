c64bb35d617a5adfd665214ce5d22aef1bc2527e
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-10-07 11:29:35 -0700

    add adn remove item to/from cart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ProductContext } from '../src/contexts/ProductContext';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
   const [products] = useState(data);
@@ -16,14 +17,19 @@ function App() {
     // add the given item to the cart
     setCart([...cart, item]);
   };
+  const removeItem = itemTitle => {
+    setCart(cart.filter(ele => ele.title !== itemTitle));
+  };
 
   return (
     <div className='App'>
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
-        {/* Routes */}
-        <Route exact path='/' component={Products} />} />
-        <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={{ cart, removeItem }}>
+          <Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path='/' component={Products} />
+          <Route path='/cart' component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
