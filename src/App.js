86d5c3964a5be7d67bdd674e477a86c665865fad
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-10-07 10:39:14 -0700

    completed step 1-4

--- src/App.js
+++ src/App.js
@@ -19,16 +19,10 @@ function App() {
 
   return (
     <div className='App'>
-      <ProductContext.Provider value={{}}>
+      <ProductContext.Provider value={{ products, addItem }}>
         <Navigation cart={cart} />
-
         {/* Routes */}
-        <Route
-          exact
-          path='/'
-          render={() => <Products products={products} addItem={addItem} />}
-        />
-
+        <Route exact path='/' component={Products} />} />
         <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
       </ProductContext.Provider>
     </div>
